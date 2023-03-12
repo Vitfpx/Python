36ecff89ce1768af7c5565c40aef99b67aa73868
Vitor Raimundo <vitorbarbosaraimundo68@gmail.com>
2023-03-12 13:24:33 -0300

    Changes ex 16

--- Ex_college/Ex16.py
+++ Ex_college/Ex16.py
@@ -2,11 +2,11 @@
 16. Write a program that solves the following problem: a “xerox” copy costs R$ 0.25 per sheet, but over 100 sheets this value drops to R$ 0.20 per unit. Given the total number of copies, inform the amount to be paid.
 '''
 
-folha = int(input("Informe a quantidade de folhas: "))
+copias = int(input("Informe a quantidade de cópias: "))
 
-xerox = folha * 0.25
+xerox = copias * 0.25
 
-if folha > 100: 
-    xerox = 0.20 * folha
+if copias > 100: 
+    xerox = 0.20 * copias
 
 print(f"O valor a ser pago será de R${(round(xerox, 2))}")
