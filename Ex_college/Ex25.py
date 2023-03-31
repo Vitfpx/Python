41502f670376333c823593dcfc6eb69069c2f77b
Vitor Raimundo <vitorbarbosaraimundo68@gmail.com>
2023-03-31 16:22:14 -0300

    Changes and del

--- Ex_college/Ex25.py
+++ Ex_college/Ex25.py
@@ -1,5 +1,5 @@
 '''
-25. Write a program that calculates the roots of passing the 2nd degree (ax² + bx + c); the values ​​of a, b, and c are user-supplied (see proposed resolution below).
+25. Write a program that calculates the roots of passing the 2nd degree (ax² + bx + c); the values of a, b, and c are user-supplied (see proposed resolution below).
 '''
 import math
 a = float(input("Digite o valor de a: "))
