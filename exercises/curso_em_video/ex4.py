a8e49407d501931791f5515cc9e2ae20fe38cbe5
Vitor Barbosa <vitorbarbosaraimundo68@gmail.com>
2023-04-13 17:07:50 -0300

    Exercícios python (4, 5)

--- exercises/curso_em_video/ex4.py
+++ exercises/curso_em_video/ex4.py
@@ -1,8 +1,10 @@
 a = input("Digite algo: ")
 
-if a.isnumeric == True:
-    int(a)
-
 print(f"O tipo primitivo desse valor é: {type(a)}")
 print(f"É apenas espaço: {a.isspace()}")
 print(f"É um número: {a.isnumeric()}")
+print(f"É um número alfabético: {a.isalpha()}")
+print(f"É um alfanumérico: {a.isalnum()}")
+print(f"É maiúscula: {a.isupper()}")
+print(f"É minúscula: {a.islower()}")
+print(f"É capitalizada: {a.istitle()}")
