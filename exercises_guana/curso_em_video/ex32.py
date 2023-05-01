aedb1d3d67cdabf376277b069ab3ca779e891047
Vitor Barbosa <vitorbarbosaraimundo68@gmail.com>
2023-05-01 18:57:54 -0300

    Novos exercícios

--- exercises_guana/curso_em_video/ex32.py
+++ exercises_guana/curso_em_video/ex32.py
@@ -1,2 +1,15 @@
-import datetime
+from datetime import date
 
+today = date.today()
+hoje = f"{today.day:02}/{today.month:02}/{today.year:04}"
+
+ano = int(input("Que ano você quer analisar? Coloque 0 para analisar o ano atual: "))
+
+if ano == 0:
+    print(hoje)
+elif ano % 100 == 0 and ano % 400 != 0:
+    print(f"O ano {ano} NÃO BISSEXTO")
+elif ano % 4 == 0:
+    print(f"O ano {ano} é BISSEXTO")
+else:
+    print(f"O ano {ano} NÃO É BISSEXTO")
